daa4f7d0c4c4dd0b8ce2b4e38550af86abeeb63d
Komodo07 <komodo.07@outlook.com>
2021-12-19 23:29:01 -0600

    added triggers and scene management
    - Added triggers that would cause the scene to reload if you cross the finish line or fall on your head.

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float invokeDelay = 2.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            Invoke(nameof(ReloadScene), invokeDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
